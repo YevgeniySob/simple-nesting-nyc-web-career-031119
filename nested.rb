eba1029306f5b3f8784720c0a3436d6da6f1f381
Yevgeniy Sobolev <yevgeniyerot@gmail.com>
2019-02-23 02:28:38 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -81,7 +81,8 @@ def adding_matz
           :languages => ["C"]
         }
      }
-  programmer_hash.store[:yukihiro_matsumoto][:known_for] = "Ruby",[:languages] = ["LISP", "C"]
+  programmer_hash.store[:yukihiro_matsumoto][:known_for] = "Ruby"
+  [:languages] = ["LISP", "C"]
 end
 
 def changing_alan
